296e28c4697560f016aebfa4eae9985a2bd940dd
Adrien Maranville <Adrien.Maranville@gmail.com>
2015-08-31 16:23:53 -0600

    Project 1: Variables and Functions

---
+++ New Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	int myInt = 6;
+	
+	void Start () {
+		myInt = multiplyByTwo (myInt);
+		Debug.Log (myInt);
+	}
+	
+	int multiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
