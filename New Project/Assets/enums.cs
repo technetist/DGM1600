8a0259873c4088b8a1e149bc7645374b49ebcd2e
Adrien Maranville <Adrien.Maranville@gmail.com>
2015-10-26 15:33:24 -0600

    Enum Project

---
+++ New Project/Assets/enums.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enums : MonoBehaviour {
+	enum Holidays : short {Halloween, Christmas, Easter, NewYears};
+
+	void Start () {
+		Holidays myHolidays;
+		myHolidays = Holidays.Christmas;
+	}
+
+	Holidays reverseHolidays (Holidays day){
+		if (day == Holidays.Christmas)
+			day = Holidays.Easter;
+		return day;
+	}
+
+}
+
+
+/*public class enums : MonoBehaviour {
+	
+	enum Direction {North, East, South, West};
+	// Use this for initialization
+	void Start () {
+		Direction myDirection;
+		
+		myDirection = Direction.North;
+		
+		ReverseDirection (myDirection);
+	}
+	
+	Direction ReverseDirection (Direction dir)
+	{
+		if(dir == Direction.North)
+			dir = Direction.South;
+		else if(dir == Direction.South)
+			dir = Direction.North;
+		else if(dir == Direction.East)
+			dir = Direction.West;
+		else if(dir == Direction.West)
+			dir = Direction.East;
+		
+		return dir;     
+	}
+}
+*/
+
