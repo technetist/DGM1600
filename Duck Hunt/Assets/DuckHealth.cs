4ae18fb2c665538678ddbe2341aa1204a5a922d6
Adrien Maranville <Adrien.Maranville@gmail.com>
2015-11-16 15:47:10 -0700

    Instantiate and Spawn

---
+++ Duck Hunt/Assets/DuckHealth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckHealth : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.tag == "KillZone")
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Duck Hunt/Assets/DuckSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckSpawner : MonoBehaviour {
+	public GameObject duck;
+
+	// Use this for initialization
+	//void Start () {
+
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+
+	public void SpawnDuck()
+	{
+		Debug.Log("Spawn Duck");
+		Instantiate (duck, transform.position, Quaternion.identity);
+	}
+}
