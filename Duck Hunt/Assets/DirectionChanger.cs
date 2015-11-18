7f884f1fa52b90c555991acbe4de97198bc05a0c
Adrien Maranville <AdrienM@uvu.edu>
2015-11-18 16:03:25 -0700

    DuckHunt Duck Movement

---
+++ Duck Hunt/Assets/DirectionChanger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionChanger : MonoBehaviour {
+	public enum Changer {Horizontal, Vertical}
+	public Changer changer;
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+
+	void OnCollisionEnter(Collision hit)
+	{
+		if (hit.transform.tag == "Duck") 
+		{
+			DuckMovement movement = hit.gameObject.GetComponent<DuckMovement>();
+
+			if(changer == Changer.Horizontal)
+			{
+				movement.DirectionChanger(new Vector3(-1, 1, 0));
+			}
+			else if(changer == Changer.Vertical)
+			{
+				movement.DirectionChanger(new Vector3(1, -1, 0));
+			}
+		}
+	}
+}
